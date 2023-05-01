7c30f3b11c7d59a4a73d15ae619823d0663b8544
vk1408 <84286885+vk1408@users.noreply.github.com>
2023-05-01 20:31:20 +0200

    Add project files.

---
+++ pdfMergerClassLibrary/pdfMerger.cs
@@ -0,0 +1,46 @@
+﻿using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Collections.Generic;
+using System;
+
+namespace pdfMergerClassLibrary
+{
+    public class pdfMerger
+    {
+        public static string fileOnePath { get; set; }
+        public static string fileTwoPath { get; set; }
+        public static string outputPath { get; set; }
+        public static string defaultFileName { get; set; } = "merged.pdf";
+        public static bool error { get; set; } = false;
+        public static string errorMessage="Done! No error";
+        public static void mergePdfFiles ()
+        {
+            List<string> pdfFiles = new List<string>() { fileOnePath,fileTwoPath };
+            PdfDocument outPdf = new PdfDocument();
+            try
+            {
+                foreach (string pdfFile in pdfFiles)
+                {
+                    PdfDocument pdf = PdfReader.Open(pdfFile, PdfDocumentOpenMode.Import);
+                    CopyPages(pdf, outPdf);
+                }
+                outPdf.Save(outputPath + "\\" + defaultFileName);
+            }
+            catch (Exception e)
+            {
+                error = true;
+                errorMessage = e.Message;
+            }
+           
+
+        }
+        public static void CopyPages(PdfDocument from, PdfDocument to)
+        {
+            for (int i = 0; i < from.PageCount; i++)
+            {
+                to.AddPage(from.Pages[i]);
+            }
+        }
+    }
+}
---
+++ pdfMergerUI/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Windows;
+using Microsoft.Win32;
+using WinForms=System.Windows.Forms;
+using pdfMergerClassLibrary;
+
+
+namespace pdfMergerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void fileOneButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                pdfMerger.fileOnePath = openFileDialog.FileName;
+                folderOnePath.Content = openFileDialog.FileName;
+            }
+        }
+
+        private void fileTwoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                pdfMerger.fileTwoPath = openFileDialog.FileName;
+                folderTwoPath.Content = openFileDialog.FileName;
+            }
+        }
+
+        private void outputPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderBrowser = new WinForms.FolderBrowserDialog();
+            folderBrowser.ShowNewFolderButton = true;
+            WinForms.DialogResult result = folderBrowser.ShowDialog();
+            if (result == WinForms.DialogResult.OK)
+            {
+                pdfMerger.outputPath = folderBrowser.SelectedPath;
+                outputFolderPath.Content = folderBrowser.SelectedPath;
+            }
+        }
+
+        private void mergeFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            pdfMerger.mergePdfFiles();
+            if (pdfMerger.error)
+            {
+                mergeResult.Content = pdfMerger.errorMessage;
+            }
+            else
+            mergeResult.Content = "Done!";
+        }
+    }
+}
